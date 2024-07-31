830ffcb4a1d649a177631261a004141bb30608db
Sahraoui Mohammed Taher Amine <65463174+CVE-1803-96@users.noreply.github.com>
2024-07-31 12:18:05 -0700

    Update gsneaks.c

--- c/great snakes/gsneaks.c
+++ c/great snakes/gsneaks.c
@@ -1,12 +1,14 @@
-#!/usr/bin/env python3
+#include <stdio.h>
 
-import sys
-# import this
+int main() {
+    int ords[] = {81, 64, 75, 66, 70, 93, 73, 72, 1, 92, 109, 2, 84, 109, 66, 75, 70, 90, 2, 92, 79};
+    int key = 0x32; // XOR key
 
-if sys.version_info.major == 2:
-    print("You are running Python 2, which is no longer supported. Please update to Python 3.")
+    printf("Here is your flag:\n");
+    for (int i = 0; i < sizeof(ords) / sizeof(ords[0]); ++i) {
+        printf("%c", ords[i] ^ key);
+    }
+    printf("\n");
 
-ords = [81, 64, 75, 66, 70, 93, 73, 72, 1, 92, 109, 2, 84, 109, 66, 75, 70, 90, 2, 92, 79]
-
-print("Here is your flag:")
-print("".join(chr(o ^ 0x32) for o in ords))
+    return 0;
+}
