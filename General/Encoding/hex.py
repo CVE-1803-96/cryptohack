cdda7911a5252168729fdc3592edbfe7ac4666fc
Sahraoui Mohammed Taher Amin <65463174+CVE-1803-96@users.noreply.github.com>
2025-09-28 21:17:14 +0300

    Update hex.py

--- General/Encoding/hex.py
+++ General/Encoding/hex.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 def main():
-    """Main function - your code goes here"""
+    
     
     encode = bytes.fromhex("63727970746f7b596f755f77696c6c5f62655f776f726b696e675f776974685f6865785f737472696e67735f615f6c6f747d")
     decode = "".join(chr(hex_code) for hex_code in encode)
@@ -11,4 +11,5 @@ def main():
     return 0
 
 if __name__ == "__main__":
+
     main()
