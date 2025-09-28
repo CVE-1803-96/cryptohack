e855c5f5d6fed7dd385894fa070069cd6e7795e9
Sahraoui Mohammed Taher Amin <65463174+CVE-1803-96@users.noreply.github.com>
2025-09-28 21:17:25 +0300

    Update longtobyte.py

--- General/Encoding/longtobyte.py
+++ General/Encoding/longtobyte.py
@@ -2,7 +2,7 @@
 from Crypto.Util.number import *
 
 def main():
-    """Main function - your code goes here"""
+    
     
     long_n  = 11515195063862318899931685488813747395775516287289682636499965282714637259206269
     byte_n  = long_to_bytes(long_n)
@@ -12,4 +12,5 @@ def main():
     return 0
 
 if __name__ == "__main__":
+
     main()
